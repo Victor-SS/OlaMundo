c75d0293ccabd13b0a381a1223f156ff095c74f9
Victor SS <142058707+Victor-SS@users.noreply.github.com>
2024-05-15 21:42:13 -0300

    Add files via upload

---
+++ OlaMundo/OlaMundo/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace OlaMundo
+{
+    partial class frmOlaMundo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOlaMundo));
+            this.btnOlaMundo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOlaMundo
+            // 
+            this.btnOlaMundo.Location = new System.Drawing.Point(82, 68);
+            this.btnOlaMundo.Name = "btnOlaMundo";
+            this.btnOlaMundo.Size = new System.Drawing.Size(635, 331);
+            this.btnOlaMundo.TabIndex = 0;
+            this.btnOlaMundo.Text = "Clique aqui";
+            this.btnOlaMundo.UseVisualStyleBackColor = true;
+            this.btnOlaMundo.Click += new System.EventHandler(this.btnOlaMundo_Click);
+            // 
+            // frmOlaMundo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOlaMundo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmOlaMundo";
+            this.Text = "Olá Mundo!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOlaMundo;
+    }
+}
+
---
+++ OlaMundo/OlaMundo/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OlaMundo
+{
+    public partial class frmOlaMundo : Form
+    {
+        public frmOlaMundo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOlaMundo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Olá Mundo!","Título", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            string nome = "Victor Osorio";
+            int idade = 23;
+
+            MessageBox.Show("Olá "+ nome+", sua idade é "+ idade+" anos.",
+                "Nome e Idade", MessageBoxButtons.OK,
+                MessageBoxIcon.Information );
+
+            string mensagem = nome + ", sua UC12 começou!";
+
+            MessageBox.Show(mensagem, "Unidade Curricular", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
